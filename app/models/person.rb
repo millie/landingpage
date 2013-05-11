c6d70618d23a0f5d2c7213e13586cf0622624a53
Millie <millie@sandboxindustries.com>
2013-05-11 12:28:57 -0700

    lots of good stuff

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,7 +3,7 @@ class Person
   include ActiveModel::Conversion
   include ActiveModel::Validations
 
-  attr_accessor :email
+  attr_accessor :email, :zip_code
 
   validates :email, :presence => true
   validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
